1b44fe6c63cb9a53f27ba28afbc8c513050b6d64
JavierCS3 <javier.castillo239873@potros.itson.edu.mx>
2024-06-27 22:06:53 -0600

    b

---
+++ Proyecto/login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+$servername = "mysql://SG-Cinepolis-equipo-1-8696-mysql-master.servers.mongodirector.com";
+$username = "sgroot";
+$password = "CZHEl@zTU5Bnpg5K";
+$dbname = "seguridad"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT id, fullname, username, password FROM usuarios WHERE username = '$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['username'] = $username;
+            header("Location: bienvenido.php");
+        } else {
+            echo "Usuario o contraseña incorrectos.";
+        }
+    } else {
+        echo "Usuario o contraseña incorrectos.";
+    }
+}
+
+$conn->close();
+?>
---
+++ Proyecto/registro.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "mysql://SG-Cinepolis-equipo-1-8696-mysql-master.servers.mongodirector.com";
+$username = "sgroot";
+$password = "CZHEl@zTU5Bnpg5K";
+$dbname = "seguridad"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fullname = $_POST['fullname'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO usuarios (fullname, username, password) VALUES ('$fullname', '$username', '$hashed_password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registro exitoso.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
